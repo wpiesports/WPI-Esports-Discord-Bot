650b15002ba0061801c6b0156e2fe8fd1a1d054e
SamuelCurrid <samuelcurrid@gmail.com>
2021-12-30 19:21:45 -0500

    Adding keyv/redis

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv').config();
+const Keyv = require('keyv');
+const fs = require('fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
