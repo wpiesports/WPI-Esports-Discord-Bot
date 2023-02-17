6728883adf174b5c975c78140d26206b404dd146
SamuelCurrid <samuelcurrid@gmail.com>
2023-02-17 09:51:16 -0500

    Adding cog handling and basic utility cog

--- bot.py
+++ bot.py
@@ -7,12 +7,119 @@ import aiohttp
 import discord
 
 from discord.ext import commands
+from discord.ext.commands import Greedy
+from typing import Optional, Literal
+
 
 # Initialize bot
 intents = discord.Intents.default()
 intents.messages = intents.message_content = intents.guild_messages = True
-bot = commands.Bot(command_prefix=".", case_insensitive=True, intents=intents)
-bot.remove_command("help")
+bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)
+bot.remove_command('help')
+default_cogs = ['utility']
+
+
+# Cog handlers
+@bot.group(name='cogs', aliases=['c'])
+@commands.is_owner()
+async def cogs(ctx: commands.Context):
+    """
+    Command group for managing cogs
+    """
+
+
+@cogs.command(name='load')
+async def load_cog(ctx: commands.Context, cog: str):
+    """
+    Loads a cog from the cogs directory
+    """
+    await bot.load_extension(f'cogs.{cog.lower()}')
+    await ctx.send(f'Successfully added cog {cog.title()}')
+
+
+@cogs.command(name='unload')
+async def unload_cog(ctx: commands.Context, cog: str):
+    """
+    Unloads a cog
+    """
+    await bot.unload_extension(f'cogs.{cog.lower()}')
+    await ctx.send(f'Successfully removed cog {cog.title()}')
+
+
+@cogs.command(name='reload')
+async def reload_cog(ctx: commands.Context, cog: str):
+    """
+    Reloads a cog
+    """
+    await bot.unload_extension(f'cogs.{cog.lower()}')
+    await bot.load_extension(f'cogs.{cog.lower()}')
+    await ctx.send(f'Successfully reloaded cog {cog.title()}')
+
+
+@cogs.command(name='list')
+async def list_cogs(ctx: commands.Context):
+    """
+    Lists the cogs available and their current status
+    """
+    await ctx.send(f'__Loaded Cogs__\n{os.linesep.join([x.title() for x in bot.extensions])}')
+
+
+# Command syncing
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: commands.Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    """
+    Syncs the command tree, Umbras sync command
+    """
+    if not guilds:
+        # Sync current guild
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        # Copy global commands to this guild
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        # Clears commands from current guild
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}")
+        return
+
+    count = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            count += 1
+
+    await ctx.send(f"Synced the tree to {count}/{len(guilds)}.")
+
+
+@cogs.error
+@load_cog.error
+@unload_cog.error
+@reload_cog.error
+@list_cogs.error
+async def cogs_error_handler(ctx: commands.Context, error: discord.DiscordException):
+    """
+    Error handling for cog commands
+    """
+    if isinstance(error.original, commands.ExtensionAlreadyLoaded):
+        await ctx.send('This cog is already loaded')
+    elif isinstance(error.original, commands.ExtensionNotLoaded):
+        await ctx.send('This cog is not loaded')
+    elif isinstance(error.original, commands.ExtensionNotFound):
+        await ctx.send('Could not find a cog with that name')
+    elif isinstance(error.original, commands.ExtensionFailed):
+        await ctx.send('Failed to load the cog')
 
 
 # Load default events
@@ -21,12 +128,13 @@ async def on_ready():
     """
     Loads default settings
     """
-    await bot.change_presence(activity=discord.Game(name="esports", start=discord.utils.utcnow()))
+    await bot.change_presence(activity=discord.Game(name='esports', start=discord.utils.utcnow()))
     print("Logged on as {0}".format(bot.user))
 
 
+# Error Handling
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx: commands.Context, error: discord.DiscordException):
     """
     Default error handling for the bot
 
@@ -34,14 +142,14 @@ async def on_command_error(ctx, error):
     :param error: error
     """
     if isinstance(error, commands.CheckFailure) or isinstance(error, commands.MissingPermissions):
-        print("!ERROR! " + str(ctx.author.id) + " did not have permissions for " + ctx.command.name + " command")
+        print(f'!ERROR! {ctx.author.id} did not have permissions for {ctx.command.name} command')
     elif isinstance(error, commands.BadArgument):
         argument = list(ctx.command.clean_params)[len(ctx.args[2:] if ctx.command.cog else ctx.args[1:])]
-        await ctx.send("Could not find the " + argument)
+        await ctx.send(f'Could not find the {argument}')
     elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(ctx.command.name + " is missing arguments")
+        await ctx.send(f'{ctx.command.name} is missing arguments')
     elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.send("Bot is missing permissions.")
+        await ctx.send('Bot is missing permissions.')
     else:
         print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
@@ -51,6 +159,11 @@ async def on_command_error(ctx, error):
 async def main():
     async with aiohttp.ClientSession() as session:
         async with bot:
+            # Load default cogs
+            for cog in default_cogs:
+                await bot.load_extension(f'cogs.{cog}')
+                print(f'Loaded cog {cog}')
+
             bot.session = session
             await bot.start(os.environ['DISCORD_TOKEN'])
 
---
+++ cogs/utility.py
@@ -0,0 +1,79 @@
+import time
+
+import discord
+
+from discord import app_commands
+from discord.ext import commands
+from typing import Optional, Union
+
+
+class Utility(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot: commands.Bot = bot
+
+    @app_commands.command(name="ping")
+    async def ping(self, interaction: discord.Interaction) -> None:
+        """
+        Sends the total latency and websock latency
+        """
+        start = time.perf_counter()
+        await interaction.response.send_message(
+            embed=discord.Embed(title='Pinging...', color=discord.Color.yellow(), timestamp=discord.utils.utcnow()),
+            ephemeral=True
+        )
+        end = time.perf_counter()
+        duration = (end - start) * 1000
+
+        await interaction.edit_original_response(
+            embed=discord.Embed(
+                title='Pong!',
+                color=discord.Color.green(),
+                description=f'Total Latency: `{round(duration, 2)} ms`\nWebSocket Latency: `{round(self.bot.latency * 1000, 2)} ms`',
+                timestamp=discord.utils.utcnow()
+            )
+        )
+
+    def get_nested_command(self, name: str, guild: Optional[discord.Guild]) \
+            -> Optional[Union[app_commands.Command, app_commands.Group]]:
+        key, *keys = name.split(' ')
+        command = self.bot.tree.get_command(key, guild=guild) or self.bot.tree.get_command(key)
+
+        for key in keys:
+            if command is None:
+                return None
+            if isinstance(command, app_commands.Command):
+                break
+
+            command = command.get_command(key)
+
+        return command
+
+    @app_commands.command(name="help")
+    async def help(self, interaction: discord.Interaction, command: Optional[str]):
+        """
+        Provides information about commands
+        """
+        # Generic information
+        if command is None:
+            embed = discord.Embed(
+                title=self.bot.user.name,
+                color=discord.Color.green(),
+                description=f'{self.bot.description}\n\n For specific command help use `/help <command>`',
+                timestamp=discord.utils.utcnow()
+            )
+            embed.set_footer(text='Creator: Moelandblue#0909')
+            embed.set_thumbnail(url=self.bot.user.avatar.url)
+        else:
+            cmd = self.get_nested_command(command, guild=interaction.guild)
+            if cmd is None:
+                await interaction.response.send_message(f'Could not find a command named {command}', ephemeral=True)
+                return
+
+            description = (cmd.callback.__doc__ or cmd.description if isinstance(cmd, app_commands.Command) else cmd.__doc__ or cmd.description)
+            embed = discord.Embed(title=cmd.qualified_name, description=description)
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Utility(bot))
